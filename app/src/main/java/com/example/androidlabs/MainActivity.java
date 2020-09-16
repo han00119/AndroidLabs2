d8e18c0036bb9e312bc76957532588b08848036e
han00119 <han00119@algonquinlive.com>
2020-09-16 19:48:16 -0400

    save4

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView TV = findViewById(R.id.MyText);
+        Button B = findViewById(R.id.MyButton);
+        CheckBox CB = findViewById(R.id.MyCheckBox);
+        ImageButton IB = findViewById(R.id.MyImageButton);
+        Switch S = findViewById(R.id.MySwitch);
+        EditText ET = findViewById(R.id.MyEditText);
     }
 }
