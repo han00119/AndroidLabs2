d3b2e12d5beb660dd13a460880f8fd51622087df
han00119 <han00119@algonquinlive.com>
2020-09-19 16:54:01 -0400

    change

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -34,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
         Switch S = findViewById(R.id.MySwitch);
         EditText ET = findViewById(R.id.MyEditText);
 
-
-//        });
         //Toast.makeText(this,"Here is more information", Toast.LENGTH_LONG).show();
         //Snackbar.make(this, "this String is", Snackbar.LENGTH_LONG).show();
         //Snackbar.setAction("UNDO", click -> cb.setChecked(!b));
