d5caec346aab79e512f5a159d5b5a61891d0121f
han00119 <han00119@algonquinlive.com>
2020-09-19 23:09:08 -0400

    step 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main);
 
         TextView TV = findViewById(R.id.MyText);
         Button B = findViewById(R.id.MyButton);
