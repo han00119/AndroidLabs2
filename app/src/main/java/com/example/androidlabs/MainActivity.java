b4a032b58b4778fb5772cb48b7804649e14bd368
han00119 <han00119@algonquinlive.com>
2020-09-30 19:41:56 -0400

    initial commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,19 +3,22 @@ package com.example.androidlabs;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    SharedPreferences prefs=null;
+    EditText et;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        prefs = getSharedPreferences("EmailList", Context.MODE_PRIVATE);
         TextView tv = findViewById(R.id.Email_Enter);
-        EditText et = findViewById(R.id.Email_Enter2);
+        et = findViewById(R.id.Email_Enter2);
         TextView tv2 = findViewById(R.id.Password_Enter);
         EditText et2 = findViewById(R.id.Password_Enter2);
     }
@@ -32,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("key", et.getText().toString());
         super.onPause();
     }
 
