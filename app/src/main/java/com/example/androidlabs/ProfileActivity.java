5067b1b3cfbfdd0b6456da29298d6b4069444a0f
han00119 <han00119@algonquinlive.com>
2020-09-30 22:23:59 -0400

    2nd file commit

--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -3,6 +3,9 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        TextView tv1 = findViewById(R.id.tv1);
+        TextView tv2 = findViewById(R.id.tv2);
+        EditText et1 = findViewById(R.id.et1);
+        TextView tv3 = findViewById(R.id.tv3);
+        EditText et2 = findViewById(R.id.et2);
+        TextView tv4 = findViewById(R.id.tv4);
+        ImageButton im = findViewById(R.id.ib);
+        TextView tv5 = findViewById(R.id.tv5);
     }
 }
