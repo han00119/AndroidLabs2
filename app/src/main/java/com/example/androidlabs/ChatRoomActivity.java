86d815cb98ac257fdf85010348eef3c65994d5f5
han00119 <han00119@algonquinlive.com>
2020-10-06 17:23:41 -0400

    Language

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -3,6 +3,8 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class ChatRoomActivity extends AppCompatActivity {
@@ -12,6 +14,10 @@ public class ChatRoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
         ListView lv = (ListView) findViewById(R.id.LV);
+        Button b = findViewById(R.id.B);
+        EditText et = findViewById(R.id.ET);
+        Button b2 = findViewById(R.id.B2);
+
 
     }
 }
